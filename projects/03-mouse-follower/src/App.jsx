14706e3af0aaa0f2e3b5a95728fd63919173f614
Lian <liandev2000@gmail.com>
2024-05-03 14:21:21 -0500

    #

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,43 +1,76 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  const [enabled, setEnabled] = useState(false);
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
+  // ponter move
   useEffect(() => {
-    console.log("efecto", { enabled });
+    console.log('efecto', { enabled })
     const handleMove = (event) => {
       const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
       //console.log('handleMove', { clientX, clientY })
     }
-    
+
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
 
-  }, [enabled]);
+    // cleanup
+    // --> Cuando el componente se desmonta
+    // --> Cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  // Cambiar el nombre del className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar ' : 'Activar '}seguir cursor
+      </button>
+    </>
+  )
+}
+
+function App() {
+  //const [mounted, setMounted] = useState(true)
 
   return (
     <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: 'translate(0px, 0px)'
-      }}>
-
-      </div>
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? "Desactivar " : "Activar "}seguir cursor
-      </button>
+      {/* {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Alternar el componente montado FollowMouse
+      </button> */}
+      <FollowMouse />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ projects/04-react-prueba-tecnica/README.md
@@ -0,0 +1,10 @@
+### Prueba Tecnica para Juniors y Trainees de React.
+
+APIs:
+    - Facts Random: https://catfact.ninja/fact
+    - Imagen Random: https://cataas.com/
+        - EndPoint para usar: `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+
+- Recupera un hecho aleatorio de gatos de la primera API.
+- Recupera la primera palabra del hecho(fact).
+- Muestra una imagen de un gatos con la primera palabra.
